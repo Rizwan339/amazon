c5d087b74b0cab1df689129f509fbf8e968360f6
Rizwan339 <iabdullahkhanak@gmail.com>
2024-01-07 23:53:43 +0500

    AA

--- lib/controller/services/auth_services/auth_services.dart
+++ lib/controller/services/auth_services/auth_services.dart
@@ -1,1 +1,32 @@
-class AuthServices {}
+import 'dart:math' as prefix;
+import 'dart:developer';
+import 'package:amazon/view/auth_screen/otp_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+class AuthServices {
+  static receiveOTP(
+      {required BuildContext context, required String mobileNo}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    try {
+      auth.verifyPhoneNumber(
+          verificationCompleted: (PhoneAuthCredential credential) {
+            log(credential.toString());
+          },
+          verificationFailed: (FirebaseAuthException exception) {
+            log(exception.toString());
+          },
+          codeSent: (String verificationID, int? resendToken) {
+            Navigator.push(
+                context,
+                PageTransition(
+                    child: OTPScreen(mobileNumber: mobileNo),
+                    type: PageTransitionType.rightToLeft));
+          },
+          codeAutoRetrievalTimeout: (String verificationID) {});
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}

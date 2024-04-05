bef9d90eda3b9116f611de134b6aba50ed1ebb54
Rizwan339 <iabdullahkhanak@gmail.com>
2024-04-05 07:21:57 +0500

    Just checking

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:amazon/controller/provider/auth_provider/auth_provider.dart';
 import 'package:amazon/utils/theme.dart';
+import 'package:amazon/view/auth_screen/auth_screen.dart';
+import 'package:amazon/view/auth_screen/otp_screen.dart';
 import 'package:amazon/view/user/user_navbar/user_navbar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -27,9 +29,7 @@ class Amazon extends StatelessWidget {
       child: MaterialApp(
         theme: themeData,
         home: BottomNavBar(),
-        // home: OTPScreen(
-        //   mobileNumber: '+92 3059691378',
-        // ),
+        // home: AuthScreen(),
         debugShowCheckedModeBanner: false,
       ),
     );
